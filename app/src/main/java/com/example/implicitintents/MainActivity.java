e53817159722bea23fcd53432c69ef0b0b5cf20d
HakimiEya <eyahkimi@gmail.com>
2023-10-29 23:01:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //RECUPERATION NÉCESSAIRE DES COMPONENTS
+        mWebsiteEditText= (EditText) findViewById(R.id.website_edittext);
+        mLocationEditText = (EditText) findViewById(R.id.location_edittext);
+        mShareTextEditText = (EditText) findViewById(R.id.share_edittext);
+        LinearLayout home= (LinearLayout) findViewById(R.id.home);
+        home.setBackground(getResources().getDrawable(R.drawable.back));
+    }
+    public void openWebsite(View view)
+    {
+        String url = mWebsiteEditText.getText().toString();
+        // L'URL saisi par l'utilisateur est ensuite transformé en un objet Uri
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
+    }
+}
